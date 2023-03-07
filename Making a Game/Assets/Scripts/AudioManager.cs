c41439b2f866dd1445b8f76a8b3214e19a27be7e
QZsteamed <1097093986@qq.com>
2023-03-08 02:17:55 +0800

    initial commit
    基本内容填充版

--- Making a Game/Assets/Scripts/AudioManager.cs
+++ Making a Game/Assets/Scripts/AudioManager.cs
@@ -9,9 +9,11 @@ public class AudioManager : MonoBehaviour
     public AudioSource audioSource;
     public AudioClip background;
     public AudioClip wheelchair;
-    public AudioClip endAudio;
+    public AudioClip goodEndAudio;
+    public AudioClip badEndAudio;
+    public AudioClip puzzle1, puzzle2, puzzle3, puzzle4;
 
-   public void Awake()
+    public void Awake()
     {
         audioManager = this;
     }
@@ -34,9 +36,63 @@ public class AudioManager : MonoBehaviour
         audioSource.Stop();
     }
 
-    public void EndAudio()
+    public void GoodEndAudio()
     {
-        audioSource.clip = endAudio;
+        audioSource.clip = goodEndAudio;
         audioSource.Play();
+    }
+
+    public void BadEndAudio()
+    {
+        audioSource.clip = badEndAudio;
+        audioSource.Play();
+    }
+
+    public void puzzleAudio1()
+    {
+        audioSource.clip = puzzle1;
+        audioSource.Play();
+    }
+
+    public void puzzleAudio2()
+    {
+        audioSource.clip = puzzle2;
+        audioSource.Play();
+    }
+
+    public void puzzleAudio3()
+    {
+        audioSource.clip = puzzle3;
+        audioSource.Play();
+    }
+
+    public void puzzleAudio4()
+    {
+        audioSource.clip = puzzle4;
+        audioSource.Play();
+    }
+
+    public void puzzleAudio1Stop()
+    {
+        audioSource.clip = puzzle1;
+        audioSource.Stop();
+    }
+
+    public void puzzleAudio2Stop()
+    {
+        audioSource.clip = puzzle2;
+        audioSource.Stop();
+    }
+
+    public void puzzleAudio3Stop()
+    {
+        audioSource.clip = puzzle3;
+        audioSource.Stop();
+    }
+
+    public void puzzleAudio4Stop()
+    {
+        audioSource.clip = puzzle4;
+        audioSource.Stop();
     }
 }
--- Making a Game/Assets/Scripts/TextEditing.cs
+++ Making a Game/Assets/Scripts/TextEditing.cs
@@ -10,6 +10,7 @@ public class TextEditing : MonoBehaviour
     public TextMeshProUGUI Text;
     public int MapNumber;
     public Boolean Moving;
+    public Boolean Playing;
 
     // Start is called before the first frame update
     void Start()
@@ -17,11 +18,19 @@ public class TextEditing : MonoBehaviour
         Text = transform.GetComponent<TextMeshProUGUI>();
         Moving = GameObject.Find("AssetLoader").GetComponent<BundleWebLoader>().Moving;
         MapNumber = GameObject.Find("Character").GetComponent<Character>().MapNumber;
+
+        Playing = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        Moving = GameObject.Find("AssetLoader").GetComponent<BundleWebLoader>().Moving;
+        MapNumber = GameObject.Find("Character").GetComponent<Character>().MapNumber;
+
+        //UnityEngine.Debug.Log("Moving:" + Moving + "MapNumber:" + MapNumber);//测试用
+        //Text = GameObject.Find("Text").GetComponent<TextMeshProUGUI>();
+
         if (Moving)
         {
             //移动过程中显示的文字
@@ -39,6 +48,7 @@ public class TextEditing : MonoBehaviour
             }
             else if (MapNumber == 3)
             {
+                AudioManager.audioManager.puzzleAudio1Stop();
                 Text.text = "";
             }
             else if (MapNumber == 4)
@@ -51,6 +61,7 @@ public class TextEditing : MonoBehaviour
             }
             else if (MapNumber == 6)
             {
+                AudioManager.audioManager.puzzleAudio2Stop();
                 Text.text = "";
             }
             else if (MapNumber == 7)
@@ -75,6 +86,7 @@ public class TextEditing : MonoBehaviour
             }
             else if (MapNumber == 12)
             {
+                AudioManager.audioManager.puzzleAudio3Stop();
                 Text.text = "";
             }
             else if (MapNumber == 13)
@@ -83,6 +95,7 @@ public class TextEditing : MonoBehaviour
             }
             else if (MapNumber == 14)
             {
+                AudioManager.audioManager.puzzleAudio4Stop();
                 Text.text = "";
             }
             else if (MapNumber == 15)
@@ -104,10 +117,15 @@ public class TextEditing : MonoBehaviour
             }
             else if (MapNumber == 2)
             {
-                Text.text = "";
+                Text.text = "每 ?0 秒就有一人因为自杀而失去生命。";
             }
             else if (MapNumber == 3)
             {
+                if (!Playing)
+                {
+                    AudioManager.audioManager.puzzleAudio1();
+                    Playing = true;
+                }
                 Text.text = "";
             }
             else if (MapNumber == 4)
@@ -116,15 +134,20 @@ public class TextEditing : MonoBehaviour
             }
             else if (MapNumber == 5)
             {
-                Text.text = "";
+                Text.text = "目前国内 ?0 % 的综合医院没有精神科。";
             }
             else if (MapNumber == 6)
             {
+                if(!Playing)
+                {
+                    AudioManager.audioManager.puzzleAudio2();
+                    Playing = true;
+                }
                 Text.text = "";
             }
             else if (MapNumber == 7)
             {
-                Text.text = "";
+                Text.text = "抑郁症患者门诊量每年增加 ?0% 。";
             }
             else if (MapNumber == 8)
             {
@@ -144,6 +167,11 @@ public class TextEditing : MonoBehaviour
             }
             else if (MapNumber == 12)
             {
+                if (!Playing)
+                {
+                    AudioManager.audioManager.puzzleAudio3();
+                    Playing = true;
+                }
                 Text.text = "";
             }
             else if (MapNumber == 13)
@@ -152,12 +180,19 @@ public class TextEditing : MonoBehaviour
             }
             else if (MapNumber == 14)
             {
+                if (!Playing)
+                {
+                    AudioManager.audioManager.puzzleAudio4();
+                    Playing = true;
+                }
                 Text.text = "";
             }
             else if (MapNumber == 15)
             {
                 Text.text = "";
             }
+
+            Text = transform.GetComponent<TextMeshProUGUI>();
         }
     }
 }
